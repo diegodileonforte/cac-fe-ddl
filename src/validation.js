9f38b8f50a1d11117e9c917572e16d63377ba094
Diego Di Leonforte <dileonfortediego@gmail.com>
2021-10-28 19:47:41 -0300

    Add responsive and styles

---
+++ src/validation.js
@@ -0,0 +1,37 @@
+// Validación de formulario
+
+const form = document.getElementById('form')
+const firstname = document.getElementById('name').value
+const lastname = document.getElementById('lastname').value
+const email = document.getElementById('email').value
+const comment = document.getElementById('comment').value
+
+const errorMsg = []
+
+form.addEventListener('submit', (e) => {
+
+    if (firstname.value === '' || firstname.value == null) {
+        errorMsg.push('Ingresa tu nombre')
+    }
+
+    if (lastname.value === '' || lastname.value == null) {
+        errorMsg.push('Ingresa tu apellido')
+    }
+
+    (function ValidateEmail(email) {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+            return (true)
+        } else {
+            errorMsg.push('Dirección de email incorrecta')
+        }
+    })
+
+    if (comment.value === '' || comment.value == null) {
+        errorMsg.push('Ingresa una consulta o mensaje')
+    }
+
+    if (errorMsg.length > 0) {
+        e.preventDefault()
+        alert(errorMsg.join(', '))
+    }
+})
